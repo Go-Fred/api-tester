9f5330978f1b16f4be20269bb908ad2d02f92ba1
Frederick <frederick.duberville@gmail.com>
2017-09-20 09:29:37 -0400

    Create the right inputs and handlers for Update App User

--- react-ui/src/calls/UpdateAppUser.js
+++ react-ui/src/calls/UpdateAppUser.js
@@ -1,27 +1,69 @@
 import React, { Component } from "react";
 import Result from "../Result.js";
 import ParametersInput from "../ParametersInput.js";
-
+import CallPreview from "../CallPreview.js";
 
 class UpdateAppUser extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userIdPayload: null,
-      smoochUserId: null,
+      responsePayload: null,
       errorPayload: null,
       messageValue: "",
+      nameParamValue:"",
+      surnameParamValue:"",
+      emailParamValue:"",
+      signedUpAtParamValue:"",
+      isPropertiesChecked:false,
       error: false
     };
   }
 
   handleMessageChange = event => {
     this.setState({ messageValue: event.target.value });
-    console.log(this.state.messageValue);
   };
 
-  updateAppUser = () => {
-    fetch("/appuser")
+  handleNameValueChange = event => {
+    this.setState({ nameParamValue: event.target.value });
+  };
+
+  handleSurnameValueChange = event => {
+    this.setState({ surnameParamValue: event.target.value });
+  };
+
+  handleEmailValueChange = event => {
+    this.setState({ emailParamValue: event.target.value });
+  };
+
+  handleSignedUpAtValueChange = event => {
+    this.setState({ signedUpAtParamValue: event.target.value });
+  };
+
+  handlePropertiesValueChange = (JSONArray) => {
+
+    this.setState({
+      isPropertiesChecked: !this.state.isPropertiesChecked,
+    });
+
+    console.log(!this.state.isPropertiesChecked)
+
+    if(!this.state.isPropertiesChecked){
+      console.log(JSONArray[0])
+      JSONArray[0].properties = { lang: "en-ca", items: 3 }
+    }
+
+  };
+
+  updateAppUser = (JSONArray) => {
+    console.log(JSONArray)
+    fetch("/updateappuser", {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(JSONArray)
+    })
       .then(response => {
         if (!response.ok) {
           throw new Error(`status ${response.status}`);
@@ -31,8 +73,7 @@ class UpdateAppUser extends Component {
       .then(data => {
         console.log(data);
         this.setState({
-          userIdPayload: JSON.stringify(data.appUser, null, 2),
-          smoochUserId: JSON.stringify(data.appUser._id, null, 2),
+          responsePayload: JSON.stringify(data, null, 2),
           error: false
         });
       })
