0baa15c8735889806d4c47f02dc6a4c155790b6e
Frederick <frederick.duberville@gmail.com>
2017-09-24 13:49:27 -0400

    Properly render highlight through Result component

--- react-ui/src/sections/Yo.js
+++ react-ui/src/sections/Yo.js
@@ -18,48 +18,6 @@ class Yo extends Component {
 
   sendYo = () => {
     window.sendyo();
-    fetch("/yo")
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState({
-          appUserMessagePayload: JSON.stringify(data.messages, null, 2),
-          error: false
-        });
-      })
-      .catch(e => {
-        this.setState({
-          errorPayload: `API call failed: ${e}`,
-          error: true
-        });
-      });
-  };
-
-  renderHighlight = () => {
-    console.log(this.state.error);
-    if (!this.state.error) {
-      if (this.state.appUserMessagePayload) {
-        return (
-          <div>
-            <p> Webhook payload (message part):</p>
-            <Highlight className="JSON">
-              {this.state.appUserMessagePayload}
-            </Highlight>
-          </div>
-        );
-      }
-    } else {
-      return (
-        <Highlight className="JSON">
-          {"Please retry, Smooch was not ready yet..."}
-        </Highlight>
-      );
-    }
-    return;
   };
 
   render() {
@@ -73,12 +31,12 @@ class Yo extends Component {
           <span className="arrow">→</span> Smooch{" "}
           <span className="arrow">→</span> Webhook
         </h3>
-        <p>Before sending out some calls, let's try simulating a "yo" message from the user and see the payload a webhook targeting message:AppUser gets in return.</p>
+        <p>Before sending out some calls, let's try simulating a "yo" message from the user and see the payload that a webhook targeting "message:AppUser" gets in return.</p>
         <div className="call">
           <div className="button-container">
             <button onClick={this.sendYo}>Send yo</button>{" "}
           </div>
-          {this.renderHighlight()}
+          <Result data={this.state.appUserMessagePayload} title="Webhook payload (message part): " error={this.state.error} errorPayload={this.state.errorPayload}/>
         </div>
       </div>
     );
